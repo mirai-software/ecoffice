22f01fc20b998ea56cc6ec064ad950618d369ccd
Vittorio D'Alfonso <vittoriodalfonso07@gmail.com>
2024-05-28 19:41:48 +0200

    ✨ Add Onboarding Redirect Based on AsyncStorage Key
    This commit adds the @react-native-async-storage/async-storage dependency to the package.json file in the apps/mobile directory. This dependency is required for implementing AsyncStorage functionality in the app.

--- apps/mobile/app/(protected)/_layout.tsx
+++ apps/mobile/app/(protected)/_layout.tsx
@@ -1,11 +1,28 @@
 import { Stack } from "expo-router";
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect } from "react";
+import { router } from "expo-router";
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from "expo-router";
 
 export default function RootLayout() {
+  /**
+   * Checks if onboarding has been completed.
+   * If not, redirects to the onboarding screen.
+   */
+  const OnboardingCheck = async () => {
+    const onboarding = await AsyncStorage.getItem("@OnboardingIsDone");
+    if (!onboarding || onboarding === "false") {
+      router.replace("(onboarding)/onboarding");
+    }
+  };
+
+  useEffect(() => {
+    OnboardingCheck();
+  }, []);
+
   return (
     <Stack
       screenOptions={{
