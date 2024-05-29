c52579153a3aa65e831fa608c38c4d78a6b995d8
Vittorio D'Alfonso <vittoriodalfonso07@gmail.com>
2024-05-29 19:13:47 +0200

    ⚡️ display user information in profile page
    This commit modifies the profile page component to include a loading state while fetching user data from the API. It also adds the functionality to display the user's information, retrieved from the API, in the profile page. This enhancement improves the user experience by providing feedback during the data loading process and displaying relevant user information.

--- apps/mobile/app/(protected)/(tabs)/(profile)/profile.tsx
+++ apps/mobile/app/(protected)/(tabs)/(profile)/profile.tsx
@@ -1,14 +1,30 @@
 import HeaderContainer from "@/app/_header";
 import { H1 } from "@/components/ui/typography";
 import { router } from "expo-router";
-import { View } from "react-native";
+import { ActivityIndicator, View } from "react-native";
+import { api } from "@/lib/api";
+import { SafeAreaView } from "@/components/safe-area-view";
+import { Text } from "@/components/ui/text";
 
 export default function Page() {
-  return (
-    <HeaderContainer router={router}>
-      <View className="flex-1 items-center justify-center bg-background p-4 gap-y-4">
-        <H1 className="text-center">Il Mio Profilo</H1>
-      </View>
-    </HeaderContainer>
-  );
+  const { data: user, isLoading } = api.user.getUser.useQuery({});
+
+  if (isLoading) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    );
+  } else
+    return (
+      <HeaderContainer router={router}>
+        <View className="flex-1 items-center justify-center bg-background p-4 gap-y-4">
+          <H1 className="text-center">Il Mio Profilo</H1>
+          {/* print the user as json structured */}
+          <View>
+            <Text>{JSON.stringify(user, null, 2)}</Text>
+          </View>
+        </View>
+      </HeaderContainer>
+    );
 }
