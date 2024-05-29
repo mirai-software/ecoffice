d9d433a83e9a30877a171a1e28bfcca2537f1f61
Vittorio D'Alfonso <vittoriodalfonso07@gmail.com>
2024-05-29 19:13:05 +0200

    ⚡️ (DevMode): Add reset onboarding functionality
    This commit adds a new button to the profile home page that allows users to reset the onboarding process. When the button is pressed, the `ResetOnboarding` function is called, which sets the `@OnboardingIsDone` key in AsyncStorage to "false" and redirects the user to the protected page. This feature provides users with the ability to restart the onboarding process if needed.

--- apps/mobile/app/(protected)/(tabs)/(profile)/profile-home.tsx
+++ apps/mobile/app/(protected)/(tabs)/(profile)/profile-home.tsx
@@ -5,8 +5,16 @@ import { Text } from "@/components/ui/text";
 import { useSupabase } from "@/context/supabase-provider";
 import HeaderContainer from "@/app/_header";
 import { View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 export default function Shop() {
   const { signOut } = useSupabase();
+
+  const ResetOnboarding = async () => {
+    await AsyncStorage.setItem("@OnboardingIsDone", "false");
+    router.push("/(protected)");
+  };
+
   return (
     <HeaderContainer router={router}>
       <View className="flex-1 items-center justify-start bg-background p-4 gap-y-4">
@@ -58,6 +66,17 @@ export default function Shop() {
           }}
         >
           <Text>Assistenza</Text>
+        </Button>
+
+        <Button
+          className="w-full bg-yellow-400"
+          variant="default"
+          size="default"
+          onPress={() => {
+            ResetOnboarding();
+          }}
+        >
+          <Text>Reset Onboarding</Text>
         </Button>
 
         <Button
