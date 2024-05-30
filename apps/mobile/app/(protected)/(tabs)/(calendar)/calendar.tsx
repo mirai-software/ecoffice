4ac546c6f7fc0f64b4e1fe70380d333bc3e2d51c
Vittorio D'Alfonso <vittoriodalfonso07@gmail.com>
2024-05-30 10:13:18 +0200

    ✨: Add city calendar API endpoint
    This commit adds the `getCityCalendar` API endpoint to the `cityRouter` in the `city.ts` file. The endpoint retrieves the city's calendar and the associated waste types from the database. It enhances the functionality of the API by allowing users to fetch the calendar and waste type information for a specific city.

--- apps/mobile/app/(protected)/(tabs)/(calendar)/calendar.tsx
+++ apps/mobile/app/(protected)/(tabs)/(calendar)/calendar.tsx
@@ -1,29 +1,45 @@
 import { H1, Muted } from "@/components/ui/typography";
-import { View } from "react-native";
+import { ActivityIndicator, ScrollView, View } from "react-native";
 import { Button } from "@/components/ui/button";
 import { router } from "expo-router";
 import { Text } from "@/components/ui/text";
 import HeaderContainer from "@/app/_header";
+import { api } from "@/lib/api";
+import { SafeAreaView } from "@/components/safe-area-view";
 export default function Shop() {
-  return (
-    <HeaderContainer router={router}>
-      <View className="flex-1 items-center justify-center bg-background p-4 gap-y-4">
-        <H1 className="text-center">Calendar</H1>
-        <Muted className="text-center">
-          You are now authenticated and this session will persist even after
-          closing the app.
-        </Muted>
-        <Button
-          className="w-full bg-red-400"
-          variant="default"
-          size="default"
-          onPress={() => {
-            router.push("/(calendar)/Spazzatura-23231");
-          }}
-        >
-          <Text>Go to Spazzatura</Text>
-        </Button>
-      </View>
-    </HeaderContainer>
-  );
+  const { data: Calendar, isLoading } = api.city.getCityCalendar.useQuery({});
+
+  if (isLoading) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    );
+  } else
+    return (
+      <HeaderContainer router={router}>
+        <View className="flex-1 items-center justify-center bg-background p-4 gap-y-4">
+          <H1 className="text-center">Calendar</H1>
+          <Muted className="text-center">
+            You are now authenticated and this session will persist even after
+            closing the app.
+          </Muted>
+
+          <ScrollView>
+            <Text>{JSON.stringify(Calendar, null, 2)}</Text>
+          </ScrollView>
+
+          <Button
+            className="w-full bg-red-400"
+            variant="default"
+            size="default"
+            onPress={() => {
+              router.push("/(calendar)/Spazzatura-23231");
+            }}
+          >
+            <Text>Go to Spazzatura</Text>
+          </Button>
+        </View>
+      </HeaderContainer>
+    );
 }
--- packages/api/src/routers/city.ts
+++ packages/api/src/routers/city.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const cityRouter = createTRPCRouter({
   getAllcity: protectedProcedure.input(z.object({})).query(({ ctx }) => {
@@ -10,4 +10,52 @@ export const cityRouter = createTRPCRouter({
         citys.map((city) => ({ label: city.name, value: city.id }))
       );
   }),
+
+  getCityCalendar: protectedProcedure
+    .input(z.object({}))
+    .query(async ({ ctx }) => {
+      // get the user city id
+      return await ctx.db.user
+        .findUnique({
+          where: { email: ctx.session.user.email },
+          select: { city: true },
+        })
+        .then(async (userCity) => {
+          if (!userCity || userCity.city === null) {
+            throw new Error("City Relation not found");
+          } else {
+            // get the city calendar and the CalendarWasteType relation
+            return await ctx.db.city
+              .findUnique({
+                where: { id: userCity.city.id },
+                include: {
+                  calendars: {
+                    include: {
+                      wasteTypes: {
+                        include: {
+                          wasteType: true, // Include i dettagli completi di wasteType
+                        },
+                      },
+                    },
+                  },
+                },
+              })
+              .then((city) => {
+                if (!city) {
+                  throw new Error("City not found");
+                }
+                return city.calendars.map((calendar) => {
+                  return {
+                    ...calendar,
+                    wasteTypes: calendar.wasteTypes.map((wasteTypeRelation) => {
+                      return {
+                        ...wasteTypeRelation.wasteType, // Usa i dettagli completi di wasteType
+                      };
+                    }),
+                  };
+                });
+              });
+          }
+        });
+    }),
 });
