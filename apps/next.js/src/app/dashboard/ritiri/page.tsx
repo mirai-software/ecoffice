fbf5e24ca67efbf8ab5b9633ba8e80e3089086a3
Vittorio D'Alfonso <grandevx@gmail.com>
2024-07-18 12:57:32 +0200

    ✨PickRequest + Report Page Implementation

--- apps/next.js/src/app/dashboard/ritiri/page.tsx
+++ apps/next.js/src/app/dashboard/ritiri/page.tsx
@@ -1,14 +1,192 @@
-import Link from "next/link";
+"use client";
 import Container from "../../_components/container";
+import { api } from "@/trpc/react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import LoadingComponent from "@/app/_components/loading";
+import { ImageModal } from "./modals/ImageModal";
+
+export type PickRequest = {
+  phone: string | null;
+  id: string;
+  address: string;
+  userId: string;
+  type: string;
+  images: string[];
+};
+
+export const columns: ColumnDef<PickRequest>[] = [
+  {
+    accessorKey: "id",
+    header: "ID",
+    cell: ({ row }) => (
+      <div className="capitalize">
+        {(row.getValue("id") as string).slice(0, 8) + "..."}
+      </div>
+    ),
+  },
+
+  {
+    accessorKey: "address",
+    header: "Indirizzo",
+    cell: ({ row }) => (
+      <div className="">{row.getValue("address") as string}</div>
+    ),
+  },
+
+  {
+    accessorKey: "type",
+    header: "Tipo di Rifiuto",
+    cell: ({ row }) => <div className="">{row.getValue("type") as string}</div>,
+  },
+
+  {
+    accessorKey: "phone",
+    header: "Telefono",
+    cell: ({ row }) => (
+      <div className="">{("+39 " + row.getValue("phone")) as string}</div>
+    ),
+  },
+
+  {
+    accessorKey: "images",
+    header: "Immagini",
+    cell: ({ row }) => (
+      <ImageModal images={row.getValue("images") as string[]} />
+    ),
+  },
+];
 
 export default function home() {
-  return (
-    <Container>
-      <h1 className="text-2xl font-bold">Home Page</h1>
-      <p className="text-sm">This is the home page</p>
-      <Link href="/dashboard/comune" className="w-full" aria-current="page">
-        Comune
-      </Link>
-    </Container>
-  );
+  const { data, isLoading } = api.admin.getCityPickRequests.useQuery();
+  console.log(data);
+
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+
+  const table = useReactTable({
+    data: data ?? [],
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+    },
+  });
+
+  if (isLoading) {
+    return (
+      <Container>
+        <LoadingComponent />
+      </Container>
+    );
+  } else
+    return (
+      <Container>
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-full text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+        <div className="flex items-center justify-end space-x-2 py-4">
+          <div className="flex-1 text-sm text-muted-foreground">
+            {table.getFilteredSelectedRowModel().rows.length} of{" "}
+            {table.getFilteredRowModel().rows.length} row(s) selected.
+          </div>
+          <div className="space-x-2">
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              Previous
+            </Button>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              Next
+            </Button>
+          </div>
+        </div>
+      </Container>
+    );
 }
